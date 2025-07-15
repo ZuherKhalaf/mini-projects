ea0c9f46ebc5aa19ee967b7ffe2a01e06341e1d6
sleet23 <zuher.khalaf@stud.plus.ac.at>
2025-07-15 20:09:22 +0200

    add while-loop implmenation

---
+++ control/loop/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+  public static void main (String[] args) {
+    System.out.println("While Loop:");
+    int i = 1;
+    while (i <= 10) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
