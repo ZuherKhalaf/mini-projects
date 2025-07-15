6ddffd678d30733962315693dda42f499965e6e0
sleet23 <zuher.khalaf@stud.plus.ac.at>
2025-07-15 20:00:57 +0200

    add EnhancedForLoop.java to loop folder

---
+++ control/loop/EnhancedForLoop.java
@@ -0,0 +1,9 @@
+public class EnhancedForLoop {
+  public static void main(String[] args) {
+    System.out.println("Enhanced For Loop (For-Each):");
+    int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+    for (int num : numbers) {
+      System.out.println(num);
+    }
+  }
+}
