acc5eaad17a74bee0462b7b46211d81e94b0c8f4
sleet23 <zuher.khalaf@stud.plus.ac.at>
2025-07-16 06:29:12 +0200

    Add DoWhileLoop.java to control

---
+++ control/loop/DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+  public static void main(String[] args) {
+    System.out.println("Do-While Loop:");
+    int  i = 1;
+    do {
+      System.out.println(i);
+      i++;
+    } while (i <= 10);
+  }
+}
